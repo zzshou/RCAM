391e1e2dc4e141c56858bc6318b955710d0141db
zzzshou <zshou@connect.ust.hk>
2021-01-13 11:02:08 +0800

    read data using window slice

--- bert_based_classifier/model.py
+++ bert_based_classifier/model.py
@@ -2,14 +2,17 @@ import logging
 from typing import Any, Dict, List
 
 import torch
-from allennlp.modules import TextFieldEmbedder, Seq2VecEncoder
 from allennlp.nn import InitializerApplicator
 from torch import nn
 from allennlp.data import Vocabulary
 from allennlp.models.model import Model
-from allennlp.training.metrics import CategoricalAccuracy
 from torch.nn import Dropout
 from bert_based_classifier.metrics import Metric
+from transformers import AlbertModel, BertModel, RobertaModel, BertConfig
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from collections import OrderedDict
+import re
+import os.path as path
 
 logger = logging.getLogger(__name__)
 
@@ -35,45 +38,111 @@ class RCAMModel(Model):
     def __init__(
             self,
             vocab: Vocabulary,
-            text_field_embedder: TextFieldEmbedder,
-            seq2vec: Seq2VecEncoder,
+            pretrained_model_type,
+            pretrained_model_path,
+            embedding_dim: int,
             dropout: float = None,
             num_choices: int = 5,
+            mode: str = 'rnn',
             initializer: InitializerApplicator = InitializerApplicator()) -> None:
         super().__init__(vocab)
-        self._text_field_embedder = text_field_embedder
-        self._seq2vec = seq2vec
-        self._linear_layer = nn.Linear(in_features=self._seq2vec.get_output_dim(), out_features=1)
+        self.pretrain = pretrained_model_type
+        if pretrained_model_type == 'bert':
+            self.pretrain = 'bert'
+            self.embedding = BertModel.from_pretrained(pretrained_model_path)
+        elif pretrained_model_type == 'albert':
+            self.embedding = AlbertModel.from_pretrained(pretrained_model_path)
+        elif pretrained_model_type == 'albertxxlarge':
+            config = BertConfig.from_json_file(path.join(pretrained_model_path, 'config.json'))
+            self.embedding = AlbertModel.from_pretrained(pretrained_model_path, from_tf=True, config=config)
+        elif pretrained_model_type == 'robert':
+            self.embedding = RobertaModel.from_pretrained(pretrained_model_path)
+        elif pretrained_model_type == 'fine-tune':
+            fine_tune_model_path = '/home/data/zshou/RCAM/model-roberta-question_length_1028_2/best.th'
+            self.embedding = RobertaModel(config=BertConfig.from_pretrained(pretrained_model_path))
+            fine_tune_state_dict = torch.load(fine_tune_model_path, map_location="cuda:3")
+            new_state_dict = OrderedDict()
+            for key, value in fine_tune_state_dict.items():
+                new_key = re.sub('^embedding\.', '', key)
+                new_state_dict[new_key] = value
+            info = self.embedding.load_state_dict(new_state_dict, strict=False)
+            logger.info(info)
+        else:
+            print('pretrained model type not included')
+        self.embedding_dim = embedding_dim
+        self._dropout = None
         if dropout:
             self._dropout = Dropout(dropout)
         self._num_choices = num_choices
         self.scorer = Metric()
+        if mode == 'rnn':
+            self.encoder_mode = mode
+            self.encoder = nn.LSTM(input_size=768,
+                                   hidden_size=768,
+                                   num_layers=2,
+                                   batch_first=True,
+                                   dropout=0.33,
+                                   bidirectional=True)
+        elif mode == 'max':
+            self.encoder_mode = mode
+            self.encoder = torch.nn.MaxPool1d
+        elif mode == 'bert':
+            self.encoder_mode = mode
+            self.encoder = RobertaModel.from_pretrained(pretrained_model_path)
+        if mode == 'rnn':
+            self._linear_layer = nn.Linear(in_features=embedding_dim * 2, out_features=1)
+        else:
+            self._linear_layer = nn.Linear(in_features=embedding_dim, out_features=1)
 
         initializer(self)
 
     def forward(
             self,
-            article_with_question: Dict[str, Dict[str, torch.LongTensor]],
+            paragraph_with_question_field: Dict[str, Dict[str, torch.LongTensor]] = None,
+            article_with_question: Dict[str, Dict[str, torch.LongTensor]] = None,
             label: List[torch.Tensor] = None,
             metadata: List[Dict[str, Any]] = None,
     ) -> Dict[str, torch.Tensor]:
-        tokens = article_with_question['bert_tokens']['token_ids']
-        tokens = tokens.view(-1, tokens.size(-1))  # batch * num_choices, seq_len
-
-        mask = article_with_question['bert_tokens']['mask']
-        mask = mask.view(-1, tokens.size(-1))
-        type_ids = article_with_question['bert_tokens']['type_ids']
-        type_ids = type_ids.view(-1, tokens.size(-1))
-
-        embedded_text = self._text_field_embedder(
-            {"bert_tokens": {"token_ids": tokens, "mask": mask,
-                             "type_ids": type_ids}})  # batch * num_choices, seq_len, hidden_dim
-        embedded_text = self._seq2vec(embedded_text, mask)  # batch * num_choices, vector_dim
+        if paragraph_with_question_field is None and article_with_question is None:
+            raise ValueError
+        elif article_with_question:
+            tokens = article_with_question['bert_tokens']['token_ids']
+            mask = article_with_question['bert_tokens']['mask']
+            type_ids = article_with_question['bert_tokens']['type_ids']
+            outputs = self.embedding(
+                tokens.view(-1, tokens.size(-1)),
+                attention_mask=mask.view(-1, tokens.size(-1)),
+                token_type_ids=type_ids.view(-1, tokens.size(-1)),
+            )
+            encode_output = outputs[1]
+        else:
+            tokens = paragraph_with_question_field['bert_tokens']['token_ids']
+            batch_size = tokens.size()[0]
+            mask = paragraph_with_question_field['bert_tokens']['mask']
+            type_ids = paragraph_with_question_field['bert_tokens']['type_ids']
+            outputs = self.embedding(tokens.view(-1, tokens.size(-1)),
+                                     attention_mask=mask.view(-1, tokens.size(-1)),
+                                     token_type_ids=type_ids.view(-1, tokens.size(-1)), )
+            embedded_text = outputs[1]
+            window_length = embedded_text.size()[0] // batch_size // 5
+            if self.encoder_mode == 'max':
+                embedded_text = embedded_text.view(batch_size * 5, window_length, self.embedding_dim).transpose(2, 1)
+                encode_output = self.encoder(kernel_size=window_length)(embedded_text).transpose(2, 1)
+                encode_output = torch.squeeze(encode_output, 1)
+            elif self.encoder_mode == 'rnn':
+                embedded_text = embedded_text.view(batch_size * 5, window_length, self.embedding_dim)
+                # logger.info(embedded_text.shape)
+                encode_output, _ = self.encoder(embedded_text)
+                # logger.info(encode_output.shape)
+                encode_output = torch.mean(encode_output, 1)
+                encode_output = torch.squeeze(encode_output, 1)
+            elif self.encoder_mode == 'bert':
+                pass
 
         if self.training:
             if self._dropout:
-                embedded_text = self._dropout(embedded_text)
-            logits = self._linear_layer(embedded_text)  # batch* num_choices, num_labels
+                encode_output = self._dropout(encode_output)
+            logits = self._linear_layer(encode_output)  # batch* num_choices, num_labels
             reshaped_logits = logits.view(-1, self._num_choices)  # batch, num_choices
 
             _loss = self.scorer.update(reshaped_logits, label)
@@ -83,7 +152,7 @@ class RCAMModel(Model):
                 "loss": _loss}
         self.eval()
         with torch.no_grad():
-            logits = self._linear_layer(embedded_text)  # batch* num_choices, num_labels
+            logits = self._linear_layer(encode_output)  # batch* num_choices, num_labels
             reshaped_logits = logits.view(-1, self._num_choices)  # batch, num_choices
 
             if label is not None:
--- bert_based_classifier/reader.py
+++ bert_based_classifier/reader.py
@@ -4,11 +4,13 @@ import logging
 from typing import Dict
 
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
-from allennlp.data.fields import Field, TextField, LabelField, MetadataField, ListField
+from allennlp.data.fields import Field, TextField, LabelField, MetadataField, ListField, SequenceLabelField
 from allennlp.data.instance import Instance
 from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
-from allennlp.data.tokenizers import Tokenizer, SpacyTokenizer
+from allennlp.data.tokenizers import Tokenizer, PretrainedTransformerTokenizer, Token
 from overrides import overrides
+from transformers import RobertaTokenizer
+import stanza
 
 logger = logging.getLogger(__name__)
 
@@ -18,16 +20,26 @@ class RCAMDatasetReader(DatasetReader):
     def __init__(self,
                  tokenizer: Tokenizer = None,
                  token_indexers: Dict[str, TokenIndexer] = None,
-                 article_length_limit: int = None,
-                 question_length_limit: int = None,
+                 article_length_limit: int = 0,
+                 question_length_limit: int = 0,
                  skip_invalid_examples: bool = False,
+                 use_window_slice: bool = False,
+                 window_slice_length: int = 0,
+                 window_slice_overlap: int = 0,
+                 max_window_slice_step: int = 0,
                  lazy: bool = False):
         super().__init__(lazy)
-        self._tokenizer = tokenizer or SpacyTokenizer()
+        self._tokenizer = tokenizer or RobertaTokenizer
         self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
         self.article_length_limit = article_length_limit
         self.option_length_limit = question_length_limit
         self.skip_invalid_examples = skip_invalid_examples
+        # self.nlp_pipeline = stanza.Pipeline('en', dir='/home/data/zshou/corpus', processors='tokenize,pos,ner',
+        #                                     tokenize_pretokenized=True)
+        self.use_window_slice = use_window_slice
+        self.window_slice_length = window_slice_length
+        self.window_slice_overlap = window_slice_overlap
+        self.max_window_slice_step = max_window_slice_step
 
     @overrides
     def _read(self, file_path: str):
@@ -41,35 +53,50 @@ class RCAMDatasetReader(DatasetReader):
     @overrides
     def text_to_instance(self, article: str, option_list: list, label_list: list = None) -> Instance:
         fields: Dict[str, Field] = {}
-        metadata = {}
 
         option_token_list = []
         combine_token = []
+        if self.use_window_slice:
+            paragraphs, num_paragraphs = get_split(article, self.window_slice_length, self.window_slice_overlap,
+                                                   self.max_window_slice_step)
+            for option in option_list:
+                option_tokens = self._tokenizer.tokenize(option)
+                option_token_list.append(option_tokens)
+            for option_token in option_token_list:
+                for paragraph in paragraphs:
+                    paragraph_token = self._tokenizer.tokenize(paragraph)
+                    paragraph_with_question = self._tokenizer.add_special_tokens(paragraph_token, option_token)
+                    combine_token.append(paragraph_with_question)
+            paragraph_with_question_field = ListField(
+                [TextField(combine, self._token_indexers) for combine in combine_token])
 
-        article_tokens = self._tokenizer.tokenize(article)
-        if self.article_length_limit is not None:
-            article_tokens = article_tokens[: self.article_length_limit]
-            metadata['article_tokens'] = [token.text for token in article_tokens]
+            fields['paragraph_with_question_field'] = paragraph_with_question_field
+            if label_list is not None:
+                label_field = ListField([LabelField(label) for label in label_list])
+                fields['label'] = label_field
+            return Instance(fields)
+        else:
+            article_tokens = self._tokenizer.tokenize(article)
+            if self.article_length_limit is not None:
+                article_tokens = article_tokens[: self.article_length_limit]
+            for option in option_list:
+                option_tokens = self._tokenizer.tokenize(option)
+                if self.option_length_limit is not None:
+                    option_tokens = option_tokens[:self.option_length_limit]
+                option_token_list.append(option_tokens)
 
-        for option in option_list:
-            option_tokens = self._tokenizer.tokenize(option)
-            if self.option_length_limit is not None:
-                option_tokens = option_tokens[:self.option_length_limit]
-            option_token_list.append(option_tokens)
+                article_with_question = self._tokenizer.add_special_tokens(article_tokens, option_tokens)
+                combine_token.append(article_with_question)
 
-            article_with_question = self._tokenizer.add_special_tokens(article_tokens, option_tokens)
-            combine_token.append(article_with_question)
+            article_with_question_field = ListField(
+                [TextField(combine, self._token_indexers) for combine in combine_token])
 
-            metadata['option_tokens'] = [[token.text for token in option] for option in option_token_list]
+            fields['article_with_question'] = article_with_question_field
+            if label_list is not None:
+                label_field = ListField([LabelField(label) for label in label_list])
+                fields['label'] = label_field
 
-        article_with_question_field = ListField([TextField(combine, self._token_indexers) for combine in combine_token])
-        fields['article_with_question'] = article_with_question_field
-        if label_list is not None:
-            label_field = ListField([LabelField(label) for label in label_list])
-            fields['label'] = label_field
-        fields['metadata'] = MetadataField(metadata)
-
-        return Instance(fields)
+            return Instance(fields)
 
 
 def parse_sentence(line):
@@ -88,3 +115,36 @@ def parse_sentence(line):
         right_answer = data['label']
         label_list[right_answer] = 'true'
     return article, option_list, label_list
+
+
+def get_split(text, sep, overlap, max_step):
+    """
+    Parameters
+    ----------
+    text : str
+        text to be splited.
+    sep : int
+        length of each slice.
+    overlap : int
+        length of overlap between two slices.
+    Returns
+    -------
+    l_total : list
+        the splited text.
+
+    """
+    l_total = []
+    n = min(max(len(text.split()) // (sep - overlap), 1), max_step)
+
+    for w in range(n):
+        if w == 0:
+            l_parcial = text.split()[:sep]
+            l_total.append(" ".join(l_parcial))
+        else:
+            l_parcial = text.split()[w * (sep - overlap): w * (sep - overlap) + sep]
+            l_total.append(" ".join(l_parcial))
+    return l_total, n
+
+
+if __name__ == '__main__':
+    file = 'data/trail_data/Task_1_Imperceptibility.jsonl'
