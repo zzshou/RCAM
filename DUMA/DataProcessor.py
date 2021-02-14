88af9ad722ddbca45fbbf6d4cb53d3fc87d91007
YJiangcm <70904956+YJiangcm@users.noreply.github.com>
2021-02-14 14:41:15 +0800

    Data preprocessing

--- DUMA/DataProcessor.py
+++ DUMA/DataProcessor.py
@@ -10,19 +10,19 @@ import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer
 from torch.utils.data import TensorDataset, DataLoader
-from wiktionaryparser import WiktionaryParser
+from get_wordnet_definition import add_wordnet_definition
 import re
 
 
 class SemEvalExample(object):
-    def __init__(self, article, choice_0, choice_1, choice_2, choice_3, choice_4, label=None):
-        self.article = article
-        self.choices = ([
-            choice_0,
-            choice_1,
-            choice_2,
-            choice_3,
-            choice_4,
+    def __init__(self, passage, option_0, option_1, option_2, option_3, option_4, label=None):
+        self.passage = passage
+        self.options = ([
+            option_0,
+            option_1,
+            option_2,
+            option_3,
+            option_4,
         ])
         self.label = label
 
@@ -31,12 +31,12 @@ class SemEvalExample(object):
 
     def __repr__(self):
         attributes = [
-            "article: {}".format(self.article),
-            "choice_0: {}".format(self.choices[0]),
-            "choice_1: {}".format(self.choices[1]),
-            "choice_2: {}".format(self.choices[2]),
-            "choice_3: {}".format(self.choices[3]),
-            "choice_4: {}".format(self.choices[4]),
+            "passage: {}".format(self.passage),
+            "option_0: {}".format(self.options[0]),
+            "option_1: {}".format(self.options[1]),
+            "option_2: {}".format(self.options[2]),
+            "option_3: {}".format(self.options[3]),
+            "option_4: {}".format(self.options[4]),
         ]
 
         if self.label is not None:
@@ -46,32 +46,49 @@ class SemEvalExample(object):
 
 
 class InputFeatures(object):
-    def __init__(self, choices_features, length, label):
-        self.choices_features = [
+    def __init__(self, options_features, length, label):
+        self.options_features = [
             {"input_ids": input_ids, "input_mask": input_mask, "segment_ids": segment_ids}
-            for _, input_ids, input_mask, segment_ids in choices_features
+            for _, input_ids, input_mask, segment_ids in options_features
         ]
         self.length = length
         self.label = label
 
 
-def read_recam(path, is_labeling, add_wiki=False):
-    parser = WiktionaryParser()
-    with open(path, mode='r', encoding="utf8") as f:
-        reader = jsonlines.Reader(f)
-        examples = []
-        for line in reader:
-            example = SemEvalExample(
-                article=line['article'],
-                choice_0=generate_choice(parser, line['option_0'], line['question'], add_wiki),
-                choice_1=generate_choice(parser, line['option_1'], line['question'], add_wiki),
-                choice_2=generate_choice(parser, line['option_2'], line['question'], add_wiki),
-                choice_3=generate_choice(parser, line['option_3'], line['question'], add_wiki),
-                choice_4=generate_choice(parser, line['option_4'], line['question'], add_wiki),
-                label = int(line['label']) if is_labeling else None,
-            )
-            examples.append(example)
-        return examples
+def read_recam(path, is_labeling, add_definition=False):
+    if add_definition:
+        add_wordnet_definition_path = add_wordnet_definition(path)
+        with open(add_wordnet_definition_path, mode='r', encoding="utf8") as f:
+            reader = jsonlines.Reader(f)
+            examples = []
+            for line in reader:
+                example = SemEvalExample(
+                    passage = line['article'],
+                    option_0 = line['question'].replace('@placeholder', line['option_0']['ans'] + ' ' + line['option_0']['definitions']),
+                    option_1 = line['question'].replace('@placeholder', line['option_1']['ans'] + ' ' + line['option_1']['definitions']),
+                    option_2 = line['question'].replace('@placeholder', line['option_2']['ans'] + ' ' + line['option_2']['definitions']),
+                    option_3 = line['question'].replace('@placeholder', line['option_3']['ans'] + ' ' + line['option_3']['definitions']),
+                    option_4 = line['question'].replace('@placeholder', line['option_4']['ans'] + ' ' + line['option_4']['definitions']),
+                    label = int(line['label']) if is_labeling else None,
+                )
+                examples.append(example)
+            return examples
+    else:
+        with open(path, mode='r', encoding="utf8") as f:
+            reader = jsonlines.Reader(f)
+            examples = []
+            for line in reader:
+                example = SemEvalExample(
+                    passage = line['article'],
+                    option_0 = line['question'].replace('@placeholder', line['option_0']),
+                    option_1 = line['question'].replace('@placeholder', line['option_1']),
+                    option_2 = line['question'].replace('@placeholder', line['option_2']),
+                    option_3 = line['question'].replace('@placeholder', line['option_3']),
+                    option_4 = line['question'].replace('@placeholder', line['option_4']),
+                    label = int(line['label']) if is_labeling else None,
+                )
+                examples.append(example)
+            return examples
 
 
 def convert_examples_to_features(examples, tokenizer, max_seq_len):
@@ -80,16 +97,16 @@ def convert_examples_to_features(examples, tokenizer, max_seq_len):
     features = []
     for example_index, example in tqdm(enumerate(examples)):
 
-        article_tokens = tokenizer.tokenize(example.article)
-        choices_features = []
+        passage_tokens = tokenizer.tokenize(example.passage)
+        options_features = []
 
-        for choice_index, choice in enumerate(example.choices):
-            choice_tokens = tokenizer.tokenize(choice)
-            _truncate_seq_pair(article_tokens, choice_tokens, max_seq_len - 2)
+        for option_index, option in enumerate(example.options):
+            option_tokens = tokenizer.tokenize(option)
+            _truncate_seq_pair(passage_tokens, option_tokens, max_seq_len - 3)
 
-            length = len(article_tokens)
-            tokens = article_tokens + ["[SEP]"] + choice_tokens + ["[SEP]"]
-            segment_ids = [0] * (length + 1) + [1] * (len(choice_tokens) + 1)
+            length = len(passage_tokens)
+            tokens = ["[CLS]"] + passage_tokens + ["[SEP]"] + option_tokens + ["[SEP]"]
+            segment_ids = [0] * (length + 2) + [1] * (len(choice_tokens) + 1)
 
             input_ids = tokenizer.convert_tokens_to_ids(tokens)
             input_mask = [1] * len(input_ids)
@@ -104,10 +121,10 @@ def convert_examples_to_features(examples, tokenizer, max_seq_len):
             assert len(input_mask) == max_seq_len
             assert len(segment_ids) == max_seq_len
 
-            choices_features.append((tokens, input_ids, input_mask, segment_ids))
+            options_features.append((tokens, input_ids, input_mask, segment_ids))
 
         label = example.label
-        features.append(InputFeatures(choices_features=choices_features, length=length, label=label))
+        features.append(InputFeatures(options_features=options_features, length=length, label=label))
 
     return features
 
@@ -130,7 +147,7 @@ def _truncate_seq_pair(tokens_a, tokens_b, max_length):
 
 
 def select_field(features, field):
-    return [[choice[field] for choice in feature.choices_features] for feature in features]
+    return [[option[field] for option in feature.options_features] for feature in features]
 
 
 def convert_features_to_dataset(features, is_labeling):
@@ -147,9 +164,9 @@ def convert_features_to_dataset(features, is_labeling):
     
 
 if __name__ == "__main__":
-    train_examples = read_recam('/content/drive/My Drive/SemEval2021-task4/data/training_data/Task_1_train.jsonl', is_labeling=True, add_wiki=True)
+    train_examples = read_recam('/content/drive/My Drive/SemEval2021-task4/data/training_data/Task_1_train.jsonl', is_labeling=True, add_definition=True)
     tokenizer = AutoTokenizer.from_pretrained('albert-base-v2')
-    train_features = convert_examples_to_features(train_examples, tokenizer, max_seq_len=100)
+    train_features = convert_examples_to_features(train_examples, tokenizer, max_seq_len=150)
     train_dataset = convert_features_to_dataset(train_features, is_labeling=True)
     train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=2)
     pass
